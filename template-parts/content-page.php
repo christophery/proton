4f4ad9272cc142dddeefca02ad543665560d7be9
Chris Yee <christophery@gmail.com>
2023-02-10 13:11:15 -0500

    added page template

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -9,15 +9,22 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+<article id="post-<?php the_ID(); ?>" <?php post_class('article'); ?>>
 
-	<?php proton_post_thumbnail(); ?>
+    <header class="article-header gh-canvas">
 
-	<div class="entry-content">
-		<?php
+        <?php the_title( '<h1 class="article-title">', '</h1>' ); ?>
+
+        <?php if ( has_post_thumbnail() ) { ?>
+            <figure class="article-image">
+                <?php the_post_thumbnail('medium_large', array('class' => 'post-card-image')); ?>
+            </figure>
+        <?php } ?>
+
+    </header>
+
+    <section class="gh-content gh-canvas">
+        <?php
 		the_content();
 
 		wp_link_pages(
@@ -27,28 +34,6 @@
 			)
 		);
 		?>
-	</div><!-- .entry-content -->
+    </section>
 
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'proton' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					wp_kses_post( get_the_title() )
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
-</article><!-- #post-<?php the_ID(); ?> -->
+</article>

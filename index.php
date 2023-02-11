1b799d57b9d4aa9d147bdbb01e5541beb9daacf0
Chris Yee <christophery@gmail.com>
2023-02-11 15:10:58 -0500

    moved index pagination

--- index.php
+++ index.php
@@ -54,47 +54,45 @@ $proton_blog_description = get_bloginfo( 'description' );
 
 		<!-- {{!-- The main content area --}} -->
 		<main id="site-main" class="site-main outer">
-		<div class="inner posts">
+			<div class="inner posts">
 
-		    <div class="post-feed">
-		        <?php
-        		if ( have_posts() ) :
+			    <div class="post-feed">
+			        <?php
+	        		if ( have_posts() ) :
 
-        			if ( is_home() && ! is_front_page() ) :
-        				?>
-        				<header>
-        					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-        				</header>
-        				<?php
-        			endif;
+	        			if ( is_home() && ! is_front_page() ) :
+	        				?>
+	        				<header>
+	        					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+	        				</header>
+	        				<?php
+	        			endif;
 
-        			/* Start the Loop */
-        			while ( have_posts() ) :
-        				the_post();
+	        			/* Start the Loop */
+	        			while ( have_posts() ) :
+	        				the_post();
 
-        				/*
-        				 * Include the Post-Type-specific template for the content.
-        				 * If you want to override this in a child theme, then include a file
-        				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-        				 */
-        				get_template_part( 'template-parts/content', get_post_type() );
+	        				/*
+	        				 * Include the Post-Type-specific template for the content.
+	        				 * If you want to override this in a child theme, then include a file
+	        				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+	        				 */
+	        				get_template_part( 'template-parts/content', get_post_type() );
 
-        			endwhile;
+	        			endwhile;
 
-        			the_posts_navigation();
+	        		else :
 
-        		else :
+	        			get_template_part( 'template-parts/content', 'none' );
 
-        			get_template_part( 'template-parts/content', 'none' );
+	        		endif;
+	        		?>
+			    </div>
 
-        		endif;
-        		?>
-		    </div>
+			    <?php the_posts_navigation(); ?>
 
-		</div>
+			</div>
 		</main>
-
-		</div>
 
 	</div>
 

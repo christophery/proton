062666ae40ed690ccdeabbd01c5e5909d0b3a0a5
Chris Yee <christophery@gmail.com>
2023-02-14 16:45:40 -0500

    tweaked no image fallback

---
+++ template-parts/icons/page-doc-smile.php
@@ -0,0 +1,25 @@
+<svg width="800px" height="800px" viewBox="0 0 32 32" enable-background="new 0 0 32 32" id="_x3C_Layer_x3E_" version="1.1" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+<g id="page_x2C__document_1_">
+<g id="XMLID_1825_">
+<g id="XMLID_1826_">
+<path d="M14.5,17.5h3c0,0.83-0.67,1.5-1.5,1.5S14.5,18.33,14.5,17.5z" fill="#263238" id="XMLID_1828_"/>
+</g>
+</g>
+<g id="XMLID_1807_">
+<g id="XMLID_2993_">
+<polyline fill="none" id="XMLID_2998_" points="     21.5,1.5 4.5,1.5 4.5,30.5 27.5,30.5 27.5,7.5    " stroke="#455A64" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"/>
+<polyline fill="none" id="XMLID_2997_" points="     21.5,1.5 27.479,7.5 21.5,7.5 21.5,4    " stroke="#455A64" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"/>
+<path d="     M16,19c-0.83,0-1.5-0.67-1.5-1.5h3C17.5,18.33,16.83,19,16,19z" fill="none" id="XMLID_2996_" stroke="#455A64" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"/>
+<path d="     M22.5,16c0-0.553-0.448-1-1-1s-1,0.447-1,1" fill="none" id="XMLID_2995_" stroke="#455A64" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"/>
+<path d="     M11.5,16c0-0.553-0.448-1-1-1s-1,0.447-1,1" fill="none" id="XMLID_2994_" stroke="#455A64" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"/>
+</g>
+<g id="XMLID_2987_">
+<polyline fill="none" id="XMLID_2992_" points="     21.5,1.5 4.5,1.5 4.5,30.5 27.5,30.5 27.5,7.5    " stroke="#263238" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"/>
+<polyline fill="none" id="XMLID_2991_" points="     21.5,1.5 27.479,7.5 21.5,7.5 21.5,4    " stroke="#263238" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"/>
+<path d="     M16,19c-0.83,0-1.5-0.67-1.5-1.5h3C17.5,18.33,16.83,19,16,19z" fill="none" id="XMLID_2990_" stroke="#263238" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"/>
+<path d="     M22.5,16c0-0.553-0.448-1-1-1s-1,0.447-1,1" fill="none" id="XMLID_2989_" stroke="#263238" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"/>
+<path d="     M11.5,16c0-0.553-0.448-1-1-1s-1,0.447-1,1" fill="none" id="XMLID_2988_" stroke="#263238" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10"/>
+</g>
+</g>
+</g>
+</svg>

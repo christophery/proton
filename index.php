ee2ed86f87aee3910c4107d67da66f7d3569ea51
Chris Yee <christophery@gmail.com>
2024-06-22 22:02:43 -0400

    removed unused h1

--- index.php
+++ index.php
@@ -61,14 +61,6 @@ if( is_front_page() ) {
 			        <?php
 	        		if ( have_posts() ) :
 
-	        			if ( is_home() && ! is_front_page() ) :
-	        				?>
-	        				<header>
-	        					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-	        				</header>
-	        				<?php
-	        			endif;
-
 	        			/* Start the Loop */
 	        			while ( have_posts() ) :
 	        				the_post();
